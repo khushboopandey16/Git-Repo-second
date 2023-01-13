6d5478883b802c77437da3b07dfb4deb8d024133
Khushboo <pandeykhushboobist@gmail.com>
2023-01-13 01:44:03 -0800

    changed heading unit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const heading = document.querySelector("#header");
 heading.addEventListener("mouseover", function (event) {
-  heading.style.fontSize = "25px";
+  heading.style.fontSize = "2.5em";
 });
